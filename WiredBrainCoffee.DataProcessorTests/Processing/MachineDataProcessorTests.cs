83c0417f9e72175baa5a3efed79dfb7c94d10151
Vasile <vasea.gaspar@gmail.com>
2024-06-05 22:42:45 +0200

    module 4 - 	Writing Testable Code
    - Test the MachineDataProcessor;
    - Know How to Decouple Dependencies (WIP)

---
+++ WiredBrainCoffee.DataProcessorTests/Processing/MachineDataProcessorTests.cs
@@ -0,0 +1,25 @@
+﻿using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Processing;
+
+public class MachineDataProcessorTests
+{
+    [Fact]
+    public void ShouldSaveCountPerCoffeeType()
+    { 
+        // Arrange
+        var machineDataProcessor = new MachineDataProcessor();
+        var items = new[]
+        {
+            new MachineDataItem("Cappuccino", new DateTime(2022, 10, 27, 8, 6, 4)),
+            new MachineDataItem("Cappuccino", new DateTime(2022, 10, 27, 9, 7, 4)),
+            new MachineDataItem("Espresso", new DateTime(2022, 10, 27, 10, 8, 4))
+        };
+
+        // Act
+        machineDataProcessor.ProcessItems(items);
+
+        // Assert
+
+    }
+}
