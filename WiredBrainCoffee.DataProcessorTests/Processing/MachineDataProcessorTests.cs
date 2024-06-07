10dba9ba8df65b63606fdfb46e8b04bb90b99027
Vasile <vasea.gaspar@gmail.com>
2024-06-07 15:28:17 +0200

    module 4 - Writing Testable Code
    - Write Another Test

--- WiredBrainCoffee.DataProcessorTests/Processing/MachineDataProcessorTests.cs
+++ WiredBrainCoffee.DataProcessorTests/Processing/MachineDataProcessorTests.cs
@@ -7,7 +7,7 @@ public class MachineDataProcessorTests
 {
     [Fact]
     public void ShouldSaveCountPerCoffeeType()
-    { 
+    {
         // Arrange
         var coffeeCountStore = new FakeCoffeeCountStore();
         var machineDataProcessor = new MachineDataProcessor(coffeeCountStore);
@@ -32,6 +32,30 @@ public class MachineDataProcessorTests
         Assert.Equal("Espresso", item.CoffeeType);
         Assert.Equal(1, item.Count);
 
+    }
+
+    [Fact]
+    public void ShouldClearPreviousCoffeeCount()
+    {
+        // Arrange
+        var coffeeCountStore = new FakeCoffeeCountStore();
+        var machineDataProcessor = new MachineDataProcessor(coffeeCountStore);
+        var items = new[]
+        {
+            new MachineDataItem("Cappuccino", new DateTime(2022, 10, 27, 8, 6, 4)),
+        };
+
+        // Act
+        machineDataProcessor.ProcessItems(items);
+        machineDataProcessor.ProcessItems(items);
+
+        // Assert
+        Assert.Equal(2, coffeeCountStore.SavedItems.Count);
+        foreach (var item in coffeeCountStore.SavedItems)
+        {
+            Assert.Equal("Cappuccino", item.CoffeeType);
+            Assert.Equal(1, item.Count);
+        }
     }
 }
 
